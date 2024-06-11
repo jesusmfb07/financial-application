5f4fa9761df93c8e349336222ef7a0415053ab8a
Jesus Mendoza <jesusmfb07@gmail.com>
2024-06-11 15:36:06 -0500

    feat: Scaffold App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'scaffoldApp,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
