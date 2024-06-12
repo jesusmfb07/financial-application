094ee0c65bbd20e41b2a3ec0b9ebe9955e03ae74
Jesus Mendoza <jesusmfb07@gmail.com>
2024-06-12 10:43:42 -0500

    feat: implementing navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,152 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    // final ThemeData theme = ThemeData(
-    //   scaffoldBackgroundColor: Colors.blue,
-    // );
     return MaterialApp(
       title: 'scaffoldApp',
       debugShowCheckedModeBanner: false,
-      // theme:theme,
       home: MyHomePage(title: 'scaffold App Home'),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, this.title}) : super(key: key);
 
   final String? title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Icon _corazon = Icon(Icons.favorite_border, color:Colors.white);
+  Icon _corazon = Icon(Icons.favorite_border, color: Colors.white);
   bool _liked = false;
+  int _selectedIndex = 0;
+  String textToDisplay = '0 : Home';
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(widget.title!),
-          actions: [
-            IconButton(
-                onPressed:_likedChange,
-                icon: _corazon,
-            )
-          ]
+        backgroundColor: Colors.blue,
+        title: Text(widget.title!),
+        actions: [
+          IconButton(
+            onPressed: _likedChange,
+            icon: _corazon,
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: const [
+            DrawerHeader(
+              child: Text(
+                'Menu Drawer',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Text('Enlace 1'),
+            Text('Enlace 2'),
+            Text('Enlace 3'),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              textToDisplay,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){_likedChange();},
+        onPressed: _likedChange,
         backgroundColor: Colors.red,
         icon: _corazon,
-        label: Text('Like!'),
+        label: const Text('Like!'),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50,
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.chat_bubble_outline_rounded,color:Colors.black),
-                  // text:Text('Chats', style: TextStyle(color: Colors.black)),),
-              ),
-              IconButton(
-                onPressed: null,
-                icon: Icon(Icons.insert_chart,color:Colors.black),
-                // text:Text('Chats', style: TextStyle(color: Colors.black)),),
-              ),
-
-            ],
-          )
-        )
-
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline_rounded),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: 'Reportes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendario',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        onTap: _onItemPressed,
       ),
     );
   }
-  void _likedChange(){
-    setState(() {
-      if(_liked){
-        _corazon = Icon(Icons.favorite_border, color:Colors.white);
-        _liked = false;
-      }else{
-        _corazon = Icon(Icons.favorite, color:Colors.red);
-        _liked = true;
-      }});
 
+  void _likedChange() {
+    setState(() {
+      if (_liked) {
+        _corazon = Icon(Icons.favorite_border, color: Colors.white);
+        _liked = false;
+      } else {
+        _corazon = Icon(Icons.favorite, color: Colors.red);
+        _liked = true;
+      }
+    });
+  }
+
+  void _onItemPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch(_selectedIndex)  {
+        case 0:
+          textToDisplay = '$_selectedIndex : Home';
+          break;
+        case 1:
+          textToDisplay = '1 : Reports';
+          break;
+        case 2:
+          textToDisplay = '2 : Calendar';
+          break;
+        case 3:
+          textToDisplay = '3 : Settings';
+          break;
+        default:
+          textToDisplay = '0 : Home';
+          break;
+      }
+    });
   }
 }
-
