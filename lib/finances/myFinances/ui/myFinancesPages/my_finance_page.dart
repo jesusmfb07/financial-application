1070643eb7a2436f4673b8db6652999d68de6306
Jesus Mendoza <jesusmfb07@gmail.com>
2024-06-21 12:32:37 -0500

    feat correction2

--- lib/finances/myFinances/ui/myFinancesPages/my_finance_page.dart
+++ lib/finances/myFinances/ui/myFinancesPages/my_finance_page.dart
@@ -1,23 +1,25 @@
-import 'package:exercises_flutter2/finances/egressEntry/application/use_cases/handler/queries/get_egress_entry_query.dart';
 import 'package:flutter/material.dart';
 import '../../../../Provider/domain/entities/provider_entity.dart';
 import '../../../../category/domain/entities/category_entity.dart';
 import '../../../../shared/ui/navigation_bar_page.dart';
 import '../../../egressEntry/application/ports/egress_entry_port.dart';
+import '../../../egressEntry/application/use_cases/egress_entry_use_case.dart';
 import '../../../egressEntry/application/use_cases/handler/command/create_egress_entry_command.dart';
 import '../../../egressEntry/application/use_cases/handler/command/delete_egress_entry_command.dart';
 import '../../../egressEntry/application/use_cases/handler/command/update_egress_entry_command.dart';
+import '../../../egressEntry/application/use_cases/handler/queries/get_egress_entry_query.dart';
 import '../../../egressEntry/domain/aggregates/egress_entry_aggregate.dart';
+import '../../../egressEntry/infrastructure/adapters/egress_entry_adapter.dart';
 import '../../../egressEntry/ui/egress/egress.dart';
 import '../../../incomeEntry/application/ports/income_entry_port.dart';
 import '../../../incomeEntry/application/use_cases/handler/command/create_income_entry_command.dart';
 import '../../../incomeEntry/application/use_cases/handler/command/delete_income_entry_command.dart';
 import '../../../incomeEntry/application/use_cases/handler/command/update_income_entry_command.dart';
 import '../../../incomeEntry/application/use_cases/handler/queries/get_income_entry_query.dart';
+import '../../../incomeEntry/application/use_cases/income_entry_use_case.dart';
 import '../../../incomeEntry/domain/aggregates/income_entry_aggregate.dart';
 import '../../../incomeEntry/infrastructure/adapters/income_entry_adapter.dart';
 import '../../../incomeEntry/ui/income/income.dart';
-
 
 class MyFinancesPage extends StatefulWidget {
   @override
@@ -27,23 +29,45 @@ class MyFinancesPage extends StatefulWidget {
 class _MyFinancesPageState extends State<MyFinancesPage> {
   int _selectedIndex = 0;
 
-  // Instancias necesarias para IncomePage
-  final incomeEntryPort = IncomeEntrySQLiteAdapter();
-  final createIncomeEntryUseCase = CreateIncomeEntryCommand(IncomeEntryPort as IncomeEntryPort);
-  final updateIncomeEntryUseCase = UpdateIncomeEntryCommand(IncomeEntryPort as IncomeEntryPort);
-  final deleteIncomeEntryUseCase = DeleteIncomeEntryCommand(IncomeEntryPort as IncomeEntryPort);
-  final getIncomeEntriesUseCase = GetIncomeEntriesQuery(IncomeEntryPort as IncomeEntryPort);
-  final incomeEntryAggregate = IncomeEntryAggregate(entries: []);
-  final incomeCategories = <Category>[]; // Llena esto con tu lista de categorías
+  late IncomeEntryPort incomeEntryPort;
+  late CreateIncomeEntryUseCase createIncomeEntryUseCase;
+  late UpdateIncomeEntryUseCase updateIncomeEntryUseCase;
+  late DeleteIncomeEntryUseCase deleteIncomeEntryUseCase;
+  late GetIncomeEntriesUseCase getIncomeEntriesUseCase;
+  late IncomeEntryAggregate incomeEntryAggregate;
+  late List<Category> incomeCategories;
 
-  // Instancias necesarias para EgressPage
-  final createEgressEntryUseCase = CreateEgressEntryCommand(EgressEntryPort as EgressEntryPort);
-  final updateEgressEntryUseCase = UpdateEgressEntryCommand(EgressEntryPort as EgressEntryPort);
-  final deleteEgressEntryUseCase = DeleteEgressEntryCommand(EgressEntryPort as EgressEntryPort);
-  final getEgressEntriesUseCase = GetEgressEntriesQuery(EgressEntryPort as EgressEntryPort);
-  final egressEntryAggregate = EgressEntryAggregate(entries: []);
-  final egressCategories = <Category>[]; // Llena esto con tu lista de categorías
-  final egressProviders = <Provider>[]; // Llena esto con tu lista de proveedores
+  late EgressEntryPort egressEntryPort;
+  late CreateEgressEntryUseCase createEgressEntryUseCase;
+  late UpdateEgressEntryUseCase updateEgressEntryUseCase;
+  late DeleteEgressEntryUseCase deleteEgressEntryUseCase;
+  late GetEgressEntriesUseCase getEgressEntriesUseCase;
+  late EgressEntryAggregate egressEntryAggregate;
+  late List<Category> egressCategories;
+  late List<Provider> egressProviders;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Inicialización de casos de uso y adaptadores para ingresos
+    incomeEntryPort = IncomeEntrySQLiteAdapter();
+    createIncomeEntryUseCase = CreateIncomeEntryCommand(incomeEntryPort);
+    updateIncomeEntryUseCase = UpdateIncomeEntryCommand(incomeEntryPort);
+    deleteIncomeEntryUseCase = DeleteIncomeEntryCommand(incomeEntryPort);
+    getIncomeEntriesUseCase = GetIncomeEntriesQuery(incomeEntryPort);
+    incomeEntryAggregate = IncomeEntryAggregate(entries: []);
+    incomeCategories = <Category>[]; // Asegúrate de llenar esto con tus categorías de ingresos
+
+    // Inicialización de casos de uso y adaptadores para egresos
+    egressEntryPort = EgressEntrySQLiteAdapter();
+    createEgressEntryUseCase = CreateEgressEntryCommand(egressEntryPort);
+    updateEgressEntryUseCase = UpdateEgressEntryCommand(egressEntryPort);
+    deleteEgressEntryUseCase = DeleteEgressEntryCommand(egressEntryPort);
+    getEgressEntriesUseCase = GetEgressEntriesQuery(egressEntryPort);
+    egressEntryAggregate = EgressEntryAggregate(entries: []);
+    egressCategories = <Category>[]; // Asegúrate de llenar esto con tus categorías de egresos
+  }
 
   void _onItemTapped(int index) {
     setState(() {
