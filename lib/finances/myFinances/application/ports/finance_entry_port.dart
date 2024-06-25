1ab7c06e5b343002f04a1909874ece55f8d32b38
Jesus Mendoza <jesusmfb07@gmail.com>
2024-06-25 09:24:53 -0500

    feat: implementing income

--- lib/finances/myFinances/application/ports/finance_entry_port.dart
+++
@@ -1,8 +0,0 @@
-// import '../../domain/entities/finance_entry.entity.dart';
-//
-// abstract class FinanceEntryPort {
-//   Stream<List<FinanceEntry>> getEntries();
-//   Future<void> createEntry(FinanceEntry entry);
-//   Future<void> updateEntry(FinanceEntry entry);
-//   Future<void> deleteEntry(String description);
-// }
