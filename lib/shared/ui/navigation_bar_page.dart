76c4f8174f3d05b9213b729159329bd1b3eb2a82
Jesus Mendoza <jesusmfb07@gmail.com>
2024-07-03 18:26:19 -0500

    feat: implementing static menu

--- lib/shared/ui/navigation_bar_page.dart
+++ lib/shared/ui/navigation_bar_page.dart
@@ -60,3 +60,138 @@ class CustomBottomNavigationBar extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import '../../register/ui/myRegister/register_page.dart';
+// import '../../chat/ui/pages/expense_manager_page.dart';
+// import '../../settings/ui/mySettings/settings_page.dart';
+//
+// class CustomBottomNavigationBar extends StatefulWidget {
+//   final int initialIndex;
+//
+//   CustomBottomNavigationBar({this.initialIndex = 0});
+//
+//   @override
+//   _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+// }
+//
+// class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+//   int _currentIndex;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _currentIndex = widget.initialIndex;
+//   }
+//
+//   void _onTabTapped(int index) {
+//     setState(() {
+//       _currentIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: IndexedStack(
+//         index: _currentIndex,
+//         children: [
+//           ExpenseManagerPage(),
+//           RegisterPage(),
+//           Placeholder(), // Página de calendario (Placeholder por ahora)
+//           SettingsPage(),
+//         ],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         currentIndex: _currentIndex,
+//         onTap: _onTabTapped,
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.chat_bubble_outline_rounded),
+//             label: 'Chats',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.insert_chart),
+//             label: 'Registro',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.calendar_today),
+//             label: 'Calendario',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             label: 'Ajustes',
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import '../../register/ui/myRegister/register_page.dart';
+// import '../../chat/ui/pages/expense_manager_page.dart';
+// import '../../settings/ui/mySettings/settings_page.dart';
+//
+// class CustomBottomNavigationBar extends StatefulWidget {
+//   final int initialIndex;
+//
+//   CustomBottomNavigationBar({this.initialIndex = 0});
+//
+//   @override
+//   _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+// }
+//
+// class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+//   late int _currentIndex;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _currentIndex = widget.initialIndex;
+//   }
+//
+//   void _onTabTapped(int index) {
+//     setState(() {
+//       _currentIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: IndexedStack(
+//         index: _currentIndex,
+//         children: [
+//           ExpenseManagerPage(),
+//           RegisterPage(),
+//           Placeholder(), // Página de calendario (Placeholder por ahora)
+//           SettingsPage(),
+//         ],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         currentIndex: _currentIndex,
+//         onTap: _onTabTapped,
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.chat_bubble_outline_rounded),
+//             label: 'Chats',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.insert_chart),
+//             label: 'Registro',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.calendar_today),
+//             label: 'Calendario',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             label: 'Ajustes',
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
